30a221577089b0a67df48e5ac17ac9eeb31a3258
ujangaja <ujangaja@gmail.com>
2021-09-21 19:31:10 +0700

    Setting react navigation | Tutorial CRUD React Native dengan Firebase : #3

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+// rnfe
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../pages/Home';
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
