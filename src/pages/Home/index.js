ca604a83b522c58a63bffecc0f75f58f273ff2e7
ujangaja <ujangaja@gmail.com>
2021-09-21 20:01:56 +0700

    Membuat bottom plus | Tutorial CRUD React Native dengan Firebase : #5 Membuat halaman tambah kontak

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,47 @@
 //rncs
 import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
 
 export default class Home extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.page}>
         <Text> Halaman Home </Text>
+        <View style={styles.wrapperButton}>
+          <TouchableOpacity style={styles.btnTambah}>
+            <FontAwesomeIcon icon={faPlus} size={20} color={'white'} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  wrapperButton: {
+    flex: 1,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    margin: 30,
+  },
+  btnTambah: {
+    padding: 20,
+    backgroundColor: 'skyblue',
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
