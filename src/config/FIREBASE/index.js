8da2499acffda11f6934743f13508f9f5dafb8dc
ujangaja <ujangaja@gmail.com>
2021-09-21 19:43:51 +0700

    Setup Firebase | Tutorial CRUD React Native dengan Firebase : #4

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCseMI86YRkcsaA545gSOCgKal-OHDJSZs',
+  authDomain: 'crud-react-native-30da9.firebaseapp.com',
+  projectId: 'crud-react-native-30da9',
+  storageBucket: 'crud-react-native-30da9.appspot.com',
+  messagingSenderId: '1059888046623',
+  appId: '1:1059888046623:web:7a0c73a870472f00cd2b2a',
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
