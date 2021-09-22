6dceca773eb51ee9f3a94572a509e16e23c07d3c
ujangaja <ujangaja@gmail.com>
2021-09-22 18:48:47 +0700

    Membuat Input Nama| Tutorial CRUD React Native dengan Firebase : #6. Desain Tampilan Tambah Kontak

--- src/pages/TambahKontak/index.js
+++ src/pages/TambahKontak/index.js
@@ -1,12 +1,13 @@
 //rncs
 import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, TextInput} from 'react-native';
 
 export default class TambahKontak extends Component {
   render() {
     return (
       <View style={styles.pages}>
-        <Text> Tambah Kontak </Text>
+        <Text style={styles.label}> Nama : </Text>
+        <TextInput placeholder="Masukan Nama" style={styles.textInput} />
       </View>
     );
   }
@@ -16,5 +17,15 @@ const styles = StyleSheet.create({
   pages: {
     flex: 1,
     padding: 50,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
   },
 });
