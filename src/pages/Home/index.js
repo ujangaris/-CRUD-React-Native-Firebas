fee6ea006f2a966d8bc10052e4df1b2e4c66c632
ujangaja <ujangaja@gmail.com>
2021-09-27 15:54:57 +0700

    menampilkan data pada halaman home | Tutorial CRUD React Native dengan Firebase : 8. Membuat Halaman Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,51 @@ import React, {Component} from 'react';
 import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import FIREBASE from '../../config/FIREBASE';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      kontaks: {},
+      kontaksKey: [],
+    };
+  }
+
+  componentDidMount() {
+    FIREBASE.database()
+      .ref('Kontak')
+      .once('value', quertSnapShot => {
+        let data = quertSnapShot.val() ? quertSnapShot.val() : {};
+        let kontakItem = {...data};
+
+        this.setState({
+          kontaks: kontakItem,
+          kontaksKey: Object.keys(kontakItem),
+        });
+      });
+  }
+
   render() {
+    // console.log("Kontkas : ", this.state.kontaks);
+    // console.log("Kontkas Key: ", this.state.kontaksKey);
+    const {kontaks, kontaksKey} = this.state;
     return (
       <View style={styles.page}>
-        <Text> Halaman Home </Text>
+        <View style={styles.header}>
+          <Text style={styles.title}>Daftar Kontak</Text>
+          <View style={styles.garis} />
+        </View>
+
+        <View style={styles.listKontak}>
+          {kontaksKey.length > 0 ? (
+            kontaksKey.map(key => <Text key={key}>{kontaks[key].nama}</Text>)
+          ) : (
+            <Text>Daftar Kosong</Text>
+          )}
+        </View>
+
         <View style={styles.wrapperButton}>
           <TouchableOpacity
             style={styles.btnTambah}
@@ -24,6 +63,22 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
+  },
+  header: {
+    paddingHorizontal: 30,
+    paddingTop: 30,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  garis: {
+    borderWidth: 1,
+    marginTop: 10,
+  },
+  listKontak: {
+    paddingHorizontal: 30,
+    marginTop: 20,
   },
   wrapperButton: {
     flex: 1,
