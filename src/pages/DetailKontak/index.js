c2e92cbc1495a496c29a119a5cb93b2c25cd9492
ujangaja <ujangaja@gmail.com>
2021-09-28 15:47:03 +0700

    menampilkan data ke halaman detail kontak | Tutorial CRUD React Native dengan Firebase : 10. Membuat Halaman Detail Kontak

--- src/pages/DetailKontak/index.js
+++ src/pages/DetailKontak/index.js
@@ -1,12 +1,34 @@
 //rncs
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import FIREBASE from '../../config/FIREBASE';
 
 export default class DetailKontak extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      kontak: {},
+    };
+  }
+
+  componentDidMount() {
+    FIREBASE.database()
+      .ref('Kontak/' + this.props.route.params.id)
+      .once('value', quertSnapShot => {
+        let data = quertSnapShot.val() ? quertSnapShot.val() : {};
+        let kontakItem = {...data};
+
+        this.setState({
+          kontak: kontakItem,
+        });
+      });
+  }
   render() {
+    const {kontak} = this.state;
     return (
       <View>
-        <Text> Detail Kontak {this.props.route.params.id} </Text>
+        <Text> Detail Kontak {kontak.nama} </Text>
       </View>
     );
   }
